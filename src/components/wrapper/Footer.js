767093b211af38f43d1e01e156ea18db48033541
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-08-11 00:15:12 -0700

    add conditionals to toggle header/footer

--- src/components/wrapper/Footer.js
+++ src/components/wrapper/Footer.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-const Footer = () => (
-  <footer className="footer">
-    <Link to="/privacy-policy" className="footer__link">Privacy Policy</Link>
-    <span>/</span>
-    <Link to="/terms-and-conditions" className="footer__link">Terms & Conditions</Link>
-  </footer>
-)
+const Footer = () => {
+  const history = useHistory();
+
+  return (
+    <footer className="footer">
+      {history.location.pathname === "/privacy-policy" ||
+      history.location.pathname === "/terms-and-conditions" ? (
+        <React.Fragment></React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Link to="/privacy-policy" className="footer__link">
+            Privacy Policy
+          </Link>
+          <span>/</span>
+          <Link to="/terms-and-conditions" className="footer__link">
+            Terms & Conditions
+          </Link>
+        </React.Fragment>
+      )}
+    </footer>
+  );
+};
 
 export default Footer;
