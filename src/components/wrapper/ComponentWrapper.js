fb3063a7399875f53f880fcd558ab6018ee42800
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-08-13 18:52:35 -0700

    add scrollTop component

--- src/components/wrapper/ComponentWrapper.js
+++ src/components/wrapper/ComponentWrapper.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import ScrollTop from "../../components/functionalities/ScrollTop";
 
 const ComponentWrapper = ({ children }) => (
   <div>
     <Header />
+    <ScrollTop />
     <section className="main">
-      <div className="main__wrapper">
-        {children}
-      </div>
+      <div className="main__wrapper">{children}</div>
     </section>
     <Footer />
   </div>
