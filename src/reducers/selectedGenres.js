0205b002fe27f29c2596434f6d03771538af8ad5
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-08-19 03:50:57 -0700

    add/define reducer

---
+++ src/reducers/selectedGenres.js
@@ -0,0 +1,10 @@
+export default (state = {}, action) => {
+  switch (action.type) {
+    case "ADD_GENRE":
+      return;
+    case "REMOVE_GENRE":
+      return;
+    default:
+      return state;
+  }
+};
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,21 +1,22 @@
-import { createStore, combineReducers, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import authReducer from '../reducers/auth';
-import otherAccountsReducer from '../reducers/otherAccounts';
-import userAccountReducer from '../reducers/userAccount';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import authReducer from "../reducers/auth";
+import otherAccountsReducer from "../reducers/otherAccounts";
+import userAccountReducer from "../reducers/userAccount";
+import selectedGenresReducer from "../reducers/selectedGenres";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
   const store = createStore(
     combineReducers({
-      auth: authReducer, // The information about authentication 
+      auth: authReducer, // The information about authentication
       userAccount: userAccountReducer, // The information of logged in user
-      otherAccounts: otherAccountsReducer // The information of other users except logged in user
+      otherAccounts: otherAccountsReducer, // The information of other users except logged in user
+      selectedGenres: selectedGenresReducer, // The information of selected genres for matching users
     }),
     composeEnhancers(applyMiddleware(thunk))
   );
 
   return store;
 };
-
