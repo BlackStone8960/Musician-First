75973dfeab6f526deaa08e2faee0b3b3a370a5dd
kit486759 <kit486759@gmail.com>
2021-09-03 14:45:57 -0700

    ssl

--- server/server.js
+++ server/server.js
@@ -2,15 +2,31 @@ const path = require('path');
 // import sslRedirect from 'heroku-ssl-redirect';
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
+var sslRedirect = require("heroku-ssl-redirect").default;
 
-// app.use(sslRedirect());
-app.use(express.static(publicPath));
+// const ssl = require('./ssl')
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+const rootDir = path.dirname(require.main.filename)
+app.use(sslRedirect())
+// console.log(ssl)
+// app.use(sslRedirect([
+//   'other',
+//   'development',
+//   'production'
+//   ]));
+
+app.use(express.static(path.join(rootDir, '../public')));
+
+app.use('/test', (req, res) => {
+  res.end(`test`);
 });
+
+app.use('*', (req, res) => {
+  console.log(path.join(rootDir, '../public', 'index.html'))
+  res.sendFile(path.join(rootDir, '../public', 'index.html'));
+});
+
 
 app.listen(port, () => {
   console.log('Server is up!');
---
+++ server/ssl.js
@@ -0,0 +1,3 @@
+// import sslRedirect from 'heroku-ssl-redirect';
+// const ssl = sslRedirect()
+// exports. = ssl
