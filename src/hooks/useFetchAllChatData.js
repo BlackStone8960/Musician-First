92cf5d45094473c200736006abac8a011f216616
Taichi I <fng.trf.kk1020@gmail.com>
2021-09-03 18:01:32 -0700

    commented out one custom hook unused

--- src/hooks/useFetchAllChatData.js
+++ src/hooks/useFetchAllChatData.js
@@ -1,36 +1,36 @@
-import { useState, useEffect } from 'react';
-import database from '../firebase/firebase';
+// import { useState, useEffect } from 'react';
+// import database from '../firebase/firebase';
 
-const useFetchAllChatData = (id1, id2) => {
-  let ref = null;
-  const [orderedRef, setOrderedRef] = useState(null);
-  const [data, setData] = useState();
+// const useFetchAllChatData = (id1, id2) => {
+//   let ref = null;
+//   const [orderedRef, setOrderedRef] = useState(null);
+//   const [data, setData] = useState();
 
-  // check if chat id exists or not
-  database.ref("/messages").once("value").then(snapshot => {
-    if (snapshot.child(`${id1}_${id2}`).exists()) {
-      ref = database.ref(`/messages/${id1}_${id2}`);
-    } else if (snapshot.child(`${id2}_${id1}`).exists()) {
-      ref = database.ref(`/messages/${id2}_${id1}`);
-    }
-    setOrderedRef(ref && ref.orderByChild('createdAt').limitToLast(30));
-  });
+//   // check if chat id exists or not
+//   database.ref("/messages").once("value").then(snapshot => {
+//     if (snapshot.child(`${id1}_${id2}`).exists()) {
+//       ref = database.ref(`/messages/${id1}_${id2}`);
+//     } else if (snapshot.child(`${id2}_${id1}`).exists()) {
+//       ref = database.ref(`/messages/${id2}_${id1}`);
+//     }
+//     setOrderedRef(ref && ref.orderByChild('createdAt').limitToLast(30));
+//   });
 
-  useEffect(() => {
-    if (orderedRef) {
-      orderedRef.on('value', snapshot => {
-        if (snapshot && snapshot.val()) {
-          console.log('snapshot.val()');
-          setData(snapshot.val());
-        }
-      })
-      return () => {
-        orderedRef.off();
-      };
-    }
-  }, [orderedRef]);
+//   useEffect(() => {
+//     if (orderedRef) {
+//       orderedRef.on('value', snapshot => {
+//         if (snapshot && snapshot.val()) {
+//           console.log('snapshot.val()');
+//           setData(snapshot.val());
+//         }
+//       })
+//       return () => {
+//         orderedRef.off();
+//       };
+//     }
+//   }, [orderedRef]);
 
-  return { data };
-}
+//   return { data };
+// }
 
-export default useFetchAllChatData;
+// export default useFetchAllChatData;
