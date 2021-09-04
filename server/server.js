9d32477945cf2324123bf1becfbb518874a5ab2d
kit486759 <kit486759@gmail.com>
2021-09-03 23:39:55 -0700

    express

--- server/server.js
+++ server/server.js
@@ -3,27 +3,35 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-var sslRedirect = require("heroku-ssl-redirect").default;
-
-// const ssl = require('./ssl')
-
+const cors = require('cors')
 const rootDir = path.dirname(require.main.filename)
-app.use(sslRedirect())
-// console.log(ssl)
-// app.use(sslRedirect([
-//   'other',
-//   'development',
-//   'production'
-//   ]));
+
+app.use(cors())
+
+
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+      if (req.headers.host === 'localhost:3000')
+          return res.redirect(301, 'https://localhost:3000');
+      if (req.headers['x-forwarded-proto'] !== 'https')
+          return res.redirect('https://' + req.headers.host + req.url);
+      else
+          return next();
+  } else
+      return next();
+});
+
 
 app.use(express.static(path.join(rootDir, '../public')));
 
 app.use('/test', (req, res) => {
+  console.log(process.env.NODE_ENV)
   res.end(`test`);
 });
 
 app.use('*', (req, res) => {
   console.log(path.join(rootDir, '../public', 'index.html'))
+  console.log(process.env.NODE_ENV)
   res.sendFile(path.join(rootDir, '../public', 'index.html'));
 });
 
