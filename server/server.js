3e751b44f9e22997703d35ab737506056f3517c3
kit486759 <kit486759@gmail.com>
2021-09-05 14:30:24 -0700

    clean code

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const path = require('path');
-// import sslRedirect from 'heroku-ssl-redirect';
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const cors = require('cors')
-const rootDir = path.dirname(require.main.filename)
+const cors = require('cors');
+const rootDir = path.dirname(require.main.filename);
 
 app.use(cors())
+app.use(express.static(path.join(rootDir, '../public')));
 
 app.use((req, res, next) => {
   if (process.env.NODE_ENV === 'production') {
@@ -20,19 +20,9 @@ app.use((req, res, next) => {
       return next();
 });
 
-app.use(express.static(path.join(rootDir, '../public')));
-
-app.use('/test', (req, res) => {
-  console.log(process.env.NODE_ENV)
-  res.end(`test`);
-});
-
 app.use('*', (req, res) => {
-  console.log(path.join(rootDir, '../public', 'index.html'))
-  // console.log(process.env.NODE_ENV)
   res.sendFile(path.join(rootDir, '../public', 'index.html'));
 });
-
 
 app.listen(port, () => {
   console.log('Server is up!');
