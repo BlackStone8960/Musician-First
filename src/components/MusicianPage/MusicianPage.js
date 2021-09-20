c2e193c30152d746f03bb78475782efbc63078ba
yumi0127 <a13158y@gmail.com>
2021-09-20 09:38:03 -0700

    delete buttons

--- src/components/MusicianPage/MusicianPage.js
+++ src/components/MusicianPage/MusicianPage.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import MusicPlayer from './MusicPlayer';
-import ChatBox from './chat/ChatBox';
+import React from "react";
+import { connect } from "react-redux";
+import MusicPlayer from "./MusicPlayer";
+import ChatBox from "./chat/ChatBox";
 
 export const MusicianPage = ({ profile, id }) => (
   <div className="other-user-profile">
     <div className="name-bio">
-      <span>{profile.firstName} {profile.lastName}</span>
+      <span>
+        {profile.firstName} {profile.lastName}
+      </span>
       <span>{`${profile.occupation} Bio`}</span>
     </div>
     <div className="picture-bio">
-      <img src={profile.photoUrl} alt="user-photo" className="profile-photo"></img>
+      <img
+        src={profile.photoUrl}
+        alt="user-photo"
+        className="profile-photo"
+      ></img>
       <div className="bio-box">
         <div className="bio-sentence">{profile.bio}</div>
       </div>
-    </div>
-    <div className="button-wrapper">
-      <div className="button button-anchor button--black">Schedule a free Zoom call</div>
-      <div className="button button-anchor button--black">Hire Now</div>
     </div>
     <div className="music-box">
       <MusicPlayer songs={profile.songs} />
@@ -27,10 +29,12 @@ export const MusicianPage = ({ profile, id }) => (
 );
 
 const mapStateToProps = (state, props) => {
-  const clickedUser = state.otherAccounts.find((account) => account.id === props.match.params.id)
+  const clickedUser = state.otherAccounts.find(
+    (account) => account.id === props.match.params.id
+  );
   return {
     profile: clickedUser.profile,
-    id: clickedUser.id
+    id: clickedUser.id,
   };
 };
 
