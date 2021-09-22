49be46495fd46e451113b117030a46fd831ae6ec
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-09-22 01:24:24 -0700

    change method to orderByKey

--- src/components/MusicianPage/chat/ChatScreen.js
+++ src/components/MusicianPage/chat/ChatScreen.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import MessageLeft from './MessageLeft';
-import MessageRight from './MessageRight';
-import moment from 'moment';
-import database from '../../../firebase/firebase';
+import React, { useEffect, useState } from "react";
+import MessageLeft from "./MessageLeft";
+import MessageRight from "./MessageRight";
+import moment from "moment";
+import database from "../../../firebase/firebase";
 
 const ChatScreen = ({ uid, otherId, roomId }) => {
   const [dataList, setDataList] = useState([]);
@@ -13,23 +13,25 @@ const ChatScreen = ({ uid, otherId, roomId }) => {
     if (uid && otherId) {
       let ref = null;
       ref = database.ref(`/messages/${roomId}`);
-      setOrderedRef(ref && ref.orderByChild('createdAt').limitToLast(30));
+      setOrderedRef(ref && ref.orderByKey().limitToLast(16));
     }
   }, [uid, otherId]);
 
   useEffect(() => {
     if (orderedRef) {
-      orderedRef.on('value', snapshot => {
+      orderedRef.on("value", (snapshot) => {
         if (snapshot && snapshot.val()) {
-          if (!snapshot.child('firstTimeFlag').exists()) {
+          if (!snapshot.child("firstTimeFlag").exists()) {
             database.ref(`/messages/${roomId}/firstTimeFlag`).set(true);
             return;
           }
-          console.log('snapshot.val()');
+          console.log("snapshot.val()");
           const data = snapshot.val();
-          setDataList(Object.entries(data || {}).map(([key, value]) => ({ key, value })));
+          setDataList(
+            Object.entries(data || {}).map(([key, value]) => ({ key, value }))
+          );
         }
-      })
+      });
       return () => {
         orderedRef.off();
       };
@@ -41,12 +43,12 @@ const ChatScreen = ({ uid, otherId, roomId }) => {
   }, [dataList]);
 
   const showViewDate = (value) => {
-    const formattedCreatedAt = moment(value.createdAt).format('YYYY/MM/DD')
+    const formattedCreatedAt = moment(value.createdAt).format("YYYY/MM/DD");
     if (viewDate !== formattedCreatedAt) {
       viewDate = formattedCreatedAt;
       return viewDate;
     }
-  }
+  };
 
   return (
     <div className="chat-container">
@@ -56,19 +58,17 @@ const ChatScreen = ({ uid, otherId, roomId }) => {
           {dataList.map(({ key, value }) => (
             <React.Fragment key={`${key}`}>
               <div className="view-date">{showViewDate(value)}</div>
-              {
-                value.uid && uid !== value.uid ? (
-                  <MessageLeft {...value} />
-                ) : (
-                  <MessageRight {...value} />
-                )
-              }
+              {value.uid && uid !== value.uid ? (
+                <MessageLeft {...value} />
+              ) : (
+                <MessageRight {...value} />
+              )}
             </React.Fragment>
           ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ChatScreen;
